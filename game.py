cdd03adfedaecf3cc1f0e816f5debc369b777e06
njeri waweru <wawerunjeri714@gmail.com>
2022-02-10 06:31:46 +0300

    added images folder with two images

--- game.py
+++ game.py
@@ -6,5 +6,18 @@ screen_width = 1000
 screen_height = 1000
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.dispaly.set_caption('JWorld')
+pygame.display.set_caption('JWorld')
+
+
+
+#load images
+sun
+run = True
+while run == True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
 
