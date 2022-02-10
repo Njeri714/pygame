40396aa74268b3b2161809c589aa4d01a217ecd7
njeri waweru <wawerunjeri714@gmail.com>
2022-02-10 06:16:28 +0300

    created my_env + game.py file and added some code

---
+++ game.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.dispaly.set_caption('JWorld')
+
